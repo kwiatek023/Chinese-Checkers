da157a22d3bdb731ab645a139fa9d3e2884ca0f5
stokrotka24 <hanna.stempniewicz@gmail.com>
2020-12-13 13:59:07 +0100

    Setting GameController

--- Server/src/main/java/server/Game.java
+++ Server/src/main/java/server/Game.java
@@ -14,91 +14,86 @@ import java.util.Scanner;
 import java.util.concurrent.Executors;
 
 public class Game {
-    private ServerSocket socket;
-    private int noConnectedPlayers;
-    private List<String> colors = new ArrayList<>(Arrays.asList("GREEN"));
-    private int noPlayers;
-    private List<Player> players;
-    private Board board;
-    private GameController controller;
+  private ServerSocket socket;
+  private int noConnectedPlayers;
+  private List<String> colors = new ArrayList<>(Arrays.asList("red", "yellow", "blue", "green", "white", "black"));
+  private GameVariant gameVariant;
+  private int noPlayers;
+  private List<Player> players;
 
-    public Game(ServerSocket socket) {
-        this.socket = socket;
-        this.players = createPlayers();
-        try {
-            start();
-        } catch (IOException e) {
+  public Game(ServerSocket socket) {
+    this.socket = socket;
+    this.players = createPlayers();
+    try {
+      start();
+    } catch (IOException e) {
 
-        }
+    }
+  }
+
+  private List<Player> createPlayers() {
+    return new ArrayList<>();
+  }
+
+  private void start() throws IOException {
+    Player owner = new Player(socket.accept(), colors.get(0));
+    players.add(owner);
+    noConnectedPlayers = 1;
+
+    owner.protocol.sendHandshake("OWNER");
+
+    if (owner.input.hasNextLine()) {
+      gameVariant = new ConcreteVariantFactory().getGameVariant(owner.input.nextLine());
     }
 
-    private List<Player> createPlayers() {
-        return new ArrayList<>();
+    if (owner.input.hasNextLine()) {
+      noPlayers = Integer.parseInt(owner.input.nextLine());
+
+      if (noPlayers < 2 || noPlayers == 5 || noPlayers > 6) {
+        throw new IllegalArgumentException("Invalid number of players.");
+      }
     }
 
-    private void start() throws IOException {
-        GameVariant gameVariant;
-        Player owner = new Player(socket.accept(), colors.get(0));
-        players.add(owner);
-        noConnectedPlayers = 1;
+    while (noConnectedPlayers < noPlayers) {
+      var player = new Player(socket.accept(), colors.get(noConnectedPlayers));
+      player.protocol.sendHandshake("PLAYER");
+      players.add(player);
+      noConnectedPlayers++;
+    }
+    runPlayers();
+  }
 
-        owner.protocol.sendHandshake("OWNER");
+  private void runPlayers() {
+    var pool = Executors.newFixedThreadPool(6);
 
-        if (owner.input.hasNextLine()) {
-            gameVariant = new ConcreteVariantFactory().getGameVariant(owner.input.nextLine());
-        }
+    for (Player player: players) {
+      pool.execute(player);
+    }
+  }
 
-        if (owner.input.hasNextLine()) {
-            noPlayers = Integer.parseInt(owner.input.nextLine());
-            board = new Board(gameVariant, noPlayers);
-            controller = new GameController(board);
-//      if (noPlayers < 2 || noPlayers == 5 || noPlayers > 6) {
-//        throw new IllegalArgumentException("Invalid number of players.");
-//      }
-        }
+  class Player implements Runnable {
+    private Scanner input = null;
+    private PrintWriter output = null;
+    private Socket socket;
+    private String color;
+    Protocol protocol;
 
-        matchColors(noPlayers);
+    public Player(Socket socket, String color) {
+      this.socket = socket;
+      this.color = color;
 
-        while (noConnectedPlayers < noPlayers) {
-            var player = new Player(socket.accept(), colors.get(noConnectedPlayers));
-            player.protocol.sendHandshake("PLAYER");
-            players.add(player);
-            noConnectedPlayers++;
-        }
-        runPlayers();
+      try {
+        input = new Scanner(socket.getInputStream());
+        output = new PrintWriter(socket.getOutputStream(), true);
+        protocol = new Protocol(output);
+      } catch (IOException e) {
+        System.out.println("Game: error occurred while creating new player.");
+      }
     }
 
-    private void runPlayers() {
-        var pool = Executors.newFixedThreadPool(6);
-
-        for (Player player : players) {
-            pool.execute(player);
-        }
+    @Override
+    public void run() {
+      protocol.welcome(color);
     }
-
-    class Player implements Runnable {
-        private Scanner input = null;
-        private PrintWriter output = null;
-        private Socket socket;
-        private String color;
-        Protocol protocol;
-
-        public Player(Socket socket, String color) {
-            this.socket = socket;
-            this.color = color;
-
-            try {
-                input = new Scanner(socket.getInputStream());
-                output = new PrintWriter(socket.getOutputStream(), true);
-                protocol = new Protocol(output);
-            } catch (IOException e) {
-                System.out.println("Game: error occurred while creating new player.");
-            }
-        }
-
-        @Override
-        public void run() {
-            protocol.welcome(color);
-        }
-    }
+  }
 }
