e29a5d4892c5b42e67de3273bff9e12735719591
stokrotka24 <hanna.stempniewicz@gmail.com>
2020-12-13 22:48:50 +0100

    Create all fields

--- Client/src/main/java/techprog/Board/Board.java
+++ Client/src/main/java/techprog/Board/Board.java
@@ -9,7 +9,8 @@ public class Board extends AbstractBoard {
 
         noRows = 17;
         noFieldsInRow = new int[] {1, 2, 3, 4, 13, 12, 11, 10, 9, 10, 11, 12, 13, 4, 3, 2, 1};
-        noIgnoredFields = new int[] {4, 4, 4, 4, 0, 1, 2, 3, 4, 4, 4, 4, 4, 9, 10, 11, 12};
+        horizontalConstant = new int[] {4, 4, 4, 4, 0, 1, 2, 3, 4, 4, 4, 4, 4, 9, 10, 11, 12};
+        noIgnoredFields = new int[] {6, 5, 5, 4, 0, 0, 1, 1, 2, 1, 1, 0, 0, 4, 5, 5, 6};
         createFields();
     }
 
@@ -19,7 +20,7 @@ public class Board extends AbstractBoard {
         for(int i = 0; i < noRows; i++) {
             for(int j = 0; j < noFieldsInRow[i]; j++) {
                 int verticalID = i;
-                int horizontalID = noIgnoredFields[i]+j;
+                int horizontalID = horizontalConstant[i]+j;
                 fields[verticalID][horizontalID] = new Field(verticalID, horizontalID);
             }
         }
