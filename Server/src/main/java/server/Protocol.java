9d22bcd26d42db2642094ffcce689fe153d02257
kwiatek023 <kwiatecki2000@gmail.com>
2020-12-28 15:09:24 +0100

    bug fix, update Protocol

--- Server/src/main/java/server/Protocol.java
+++ Server/src/main/java/server/Protocol.java
@@ -12,17 +12,17 @@ public class Protocol {
 
   public void sendHandshake(String name) {
     output.println(name);
-    System.out.println("Handshake sent.");
+    System.out.println("Handshake sent to " + name);
   }
 
   public void welcome(String color, int noPlayers, String currentColor) {
     output.println("WELCOME " + color + " " + noPlayers + " " + currentColor);
-    System.out.println("Welcome message sent.");
+    System.out.println("Welcome message sent to " + color);
   }
 
   public void nextTurn(String color) {
     output.println("NEXT " + color);
-    System.out.println("Next turn message sent.");
+    System.out.println("Next turn message sent. Now is " + color + "' turn.");
   }
 
   public void endGame() {
@@ -32,21 +32,21 @@ public class Protocol {
 
   public void opponentMoved(int oldVerticalID, int oldHorizontalID, int newVerticalID, int newHorizontalID) {
     output.println("OPPONENT_MOVED " + oldVerticalID + " " + oldHorizontalID + " " + newVerticalID + " " + newHorizontalID);
-    System.out.println("Opponent moved message sent.");
+    System.out.println("Opponent moved message sent: " + oldVerticalID + " " + oldHorizontalID + " " + newVerticalID + " " + newHorizontalID);
   }
 
   public void validMoveMsg(int oldVerticalID, int oldHorizontalID, int newVerticalID, int newHorizontalID) {
     output.println("VALID_MOVE " + oldVerticalID + " " + oldHorizontalID + " " + newVerticalID + " " + newHorizontalID);
-    System.out.println("Valid move message sent.");
+    System.out.println("Valid move message sent: " + oldVerticalID + " " + oldHorizontalID + " " + newVerticalID + " " + newHorizontalID);
   }
 
   public void hasFinished(String color) {
     output.println("HAS_FINISHED " + color);
-    System.out.println("Has finished message sent.");
+    System.out.println("Has finished message sent. " + color + "finished.");
   }
 
   public void rageQuit(String color) {
     output.println("RAGE_QUIT " + color);
-    System.out.println("Rage quit message sent.");
+    System.out.println("Rage quit message sent. " + color + " left the game.");
   }
 }
